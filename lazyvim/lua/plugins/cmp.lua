9ebb7cdc0cac0b8c3c332fb8987f31fc8a818cc6
Fernando Tarin Morales <icemanf@gmail.com>
2023-04-07 13:21:44 +0900

    Added support for spelling completion

---
+++ lazyvim/lua/plugins/cmp.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji", "f3fora/cmp-spell" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        {
+          name = "spell",
+          option = {
+            keep_all_entries = false,
+            enable_in_context = function()
+              return true
+            end,
+          },
+        },
+      }))
+    end,
+  },
+}
