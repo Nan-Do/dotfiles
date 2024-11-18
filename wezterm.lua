34e5ffaa222722b7ed4479e332552bb48a0d786e
Fernando Tarin Morales <tarin.f@gmail.com>
2024-11-18 18:49:29 +0900

    Added a background image to wezterm (F11 goes fullscreen)

--- wezterm.lua
+++ wezterm.lua
@@ -11,23 +11,33 @@ end)
 return {
 	-- color_scheme = "Hardcore",
 	-- color_scheme = "Hopscotch.256",
-	color_scheme = "FirefoxDev",
+	-- color_scheme = "FirefoxDev",
 	-- color_scheme = "Afterglow",
 	font = wezterm.font("Hack Nerd Font"),
 	-- font = wezterm.font("JetBrains Mono Nerd Font"),
-	font_size = 14,
+	font_size = 12,
 	line_height = 1,
 	use_dead_keys = false,
 	scrollback_lines = 5000,
 	adjust_window_size_when_changing_font_size = false,
 	hide_tab_bar_if_only_one_tab = true,
 	disable_default_key_bindings = true,
-	window_background_opacity = 0.98,
+	-- window_background_opacity = 0.98,
 	-- text_background_opacity = 0.5,
+	window_background_image = "/home/deck/.config/wezterm/desert_night.jpg",
+	window_background_image_hsb = {
+		brightness = 0.025,
+	},
 	window_padding = {
 		left = 1,
 		right = 1,
 		top = 0,
 		bottom = 0,
+	},
+	keys = {
+		{
+			key = "F11",
+			action = wezterm.action.ToggleFullScreen,
+		},
 	},
 }
