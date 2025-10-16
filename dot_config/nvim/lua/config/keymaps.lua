38e0605aa26924c97b86483f0d9ef339e2b7a37d
Nando <tarin.f@gmail.com>
2025-10-16 19:05:28 +0900

    Added personal dotfiles for my personal setup including zsh, starfish, hyprland, neovim, wofi, waybar, tmux, and others

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
+
+vim.keymap.del("n", "<C-h>")
+vim.keymap.del("n", "<C-j>")
+vim.keymap.del("n", "<C-k>")
+vim.keymap.del("n", "<C-l>")
+vim.keymap.set("n", "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>")
+vim.keymap.set("n", "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>")
+vim.keymap.set("n", "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>")
+vim.keymap.set("n", "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>")
---
+++ dot_config/nvim/lua/plugins/tmux.lua
@@ -0,0 +1,9 @@
+return {
+  -- "christoomey/vim-tmux-navigator",
+  "alexghergh/nvim-tmux-navigation",
+  config = function()
+    require("nvim-tmux-navigation").setup({
+      disable_when_zoomed = true, -- defaults to false
+    })
+  end,
+}
